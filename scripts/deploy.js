6e21c187e02a2278b637f6d964c3af5cdf65072d
jasonshi10 <jasonxinshi@pm.me>
2022-01-02 20:31:52 -0800

    updated contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,22 @@
-const main = async() => {
-    const [deployer] = await hre.ethers.getSigners();
-    const accountBalance = await deployer.getBalance();
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
 
-    console.log("Deploying contracts with account: ", deployer.address);
-    console.log("Account balance: ", accountBalance.toString());
+  await waveContract.deployed();
 
-    const Token = await hre.ethers.getContractFactory("WavePortal");
-    const portal = await Token.deploy(); 
-    await portal.deployed();
-
-    console.log("WavePortal deployed address: ", portal.address);
+  console.log("WavePortal address: ", waveContract.address);
 };
 
 const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
-  
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
